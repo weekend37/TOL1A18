6bcdd219fcefdadbe2914fffbd3549255d0ba392
weekend37 <helgi37@gmail.com>
2018-11-29 13:16:18 +0000

    Create Syniprof.py

---
+++ Syniprof.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Sýniprófafjör
+@author: helgi
+"""
+# -----------------------------------------------------
+""" global: hvað prentast út? """
+
+#def helper(a,b):
+#    x = x + 1
+#    return
+#
+#x = 5
+#helper(2,3)
+#print(x)
+
+
+""" global: en hér? """
+
+#def helper(a,b):
+#    global x
+#    x = x + 1
+#    return
+#
+#x = 5
+#helper(2,3)
+#print(x)
+
+
+# -----------------------------------------------------
+""" Endurkvæmni: Hvað prentast út? """
+
+#def recursive(n):
+#    if n == 0: 
+#        return
+#    else: 
+#        print(n)
+#        recursive(n)
+#        
+#recursive(4)
+
+
+""" Endurkvæmni: En hér? """
+
+#def recursive(n):
+#    if n == 0: 
+#        return
+#    else: 
+#        print(n)
+#        recursive(n)
+#        
+#recursive(4)
+
+# -----------------------------------------------------
+""" data tree """
+# https://www.tutorialspoint.com/data_structures_algorithms/tree_data_structure.htm
+
+# -----------------------------------------------------
+
+""" one liner """
+
+#def factorial(x): return x*factorial(x-1) if x>1 else 1
+#print(factorial(10))
+
